01829beb080d2390beabdc96ea24b34c67664fec
Sudip Guha <sudipguha29@gmail.com>
2016-02-24 01:42:18 -0800

    3d plotting too!

--- needle_tracking.py
+++ needle_tracking.py
@@ -6,6 +6,8 @@ from __future__ import division
 import numpy as np
 from scipy import misc, ndimage
 import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import cv2
 import pdb
 from Tkinter import *
@@ -13,8 +15,10 @@ import tkFileDialog
 import argparse
 import random
 from skimage import measure, filters, data
-
-from color_classifier import is_metallic
+from mayavi import mlab
+from color_classifier import is_metallic, is_metallic_fast
+import pygame
+import time
 
 verbose = False
 zones = None
@@ -24,7 +28,7 @@ img_height = 0
 p_map = None
 num_windows = 0
 filename = None
-
+global components
 
 def main():
 	"""
@@ -72,9 +76,17 @@ def main():
 	zones = np.zeros(((int)(np.sqrt(num_windows)),(int)(np.sqrt(num_windows)))) # set up the zones of distinct probability
 	# start off with a uniform probability distribution
 	generate_uniform_p()
+	print "starting metal segmentation"
+	t0 = time.time()
 	add_metal_segmentation_p()
+	t1 = time.time()
+	print "done metal segmentation in "+str(t1-t0)+" s"
+	print "starting edges segmentation"
 	add_edge_segmentation_p()
+	t2 = time.time()
+	print "done edge segmentation in "+str(t2-t1)+" s"
 	plot_pixel_map()
+	plot_components()
 
 
 def add_metal_segmentation_p():
@@ -84,14 +96,14 @@ def add_metal_segmentation_p():
 	global img_width
 
 	metal_zones = np.zeros((zones.shape[0], zones.shape[1]))
-	xs = np.r_[0:img_width:1]
-	ys = np.r_[0:img_height:1]
+	xs = np.r_[0:img_width:2]
+	ys = np.r_[0:img_height:2]
 	tmp1 = img_width/np.sqrt(num_windows)
 	tmp2 = img_height/np.sqrt(num_windows)
-
+	metal_mask = is_metallic_fast(image)
 	for x in xs:
 		for y in ys:
-			if is_metallic(image[y,x]):
+			if metal_mask[y,x]:
 				metal_zones[y//tmp2,x//tmp1] += 1
 	metal_zones = metal_zones/metal_zones.sum()
 	zones = zones*metal_zones
@@ -104,6 +116,7 @@ def add_edge_segmentation_p():
 	global img_height
 	global img_width
 	global filename
+	global components
 
 	min_contour = img_height/4;
 	max_contour = 2*img_height;
@@ -116,8 +129,10 @@ def add_edge_segmentation_p():
 	components = []
 	area = len(ROI)*len(ROI[0])
 	for i in component_numbers:
-	    if np.count_nonzero(labels == i)>min_contour and np.count_nonzero(labels == i)<=max_contour:
-	        components.append((labels == i)*255)
+		l = labels==i
+		countour_size = np.count_nonzero(l)
+		if (countour_size > min_contour and countour_size <= max_contour):
+			components.append(l)
 	for i in range(len(components)):
 		components[i] = (filters.gaussian_filter(components[i].astype('float'), sigma=3)>(components[i].max()/3))*255
 	edges = sum(components)
@@ -127,8 +142,8 @@ def add_edge_segmentation_p():
 		plt.show()
 
 	edge_zones = np.zeros((zones.shape[0], zones.shape[1]))
-	xs = np.r_[0:img_width:1]
-	ys = np.r_[0:img_height:1]
+	xs = np.r_[0:img_width:2]
+	ys = np.r_[0:img_height:2]
 	tmp1 = img_width/np.sqrt(num_windows)
 	tmp2 = img_height/np.sqrt(num_windows)
 
@@ -141,6 +156,8 @@ def add_edge_segmentation_p():
 	normalize_map()
 	print zones
 
+def add_shape_segmentation():
+	pass
 
 ############################
 #### HELPER FUNCTIONS ######
@@ -152,19 +169,43 @@ def plot_pixel_map():
 	global img_height
 	global img_width
 	generate_pixel_map()
+	print "now for 3D!!!"
+	X = range(img_width)
+	Y = range(img_height)
+	print str(p_map)
+	m = mlab.surf(X,Y,p_map, warp_scale='auto', opacity=1)
+	mlab.colorbar(m, orientation='vertical')
+	mlab.axes(m)
+	mlab.xlabel('Y')
+	mlab.ylabel('X')
+	mlab.zlabel('probability')
+	mlab.outline(m)
+	mlab.show()
+	raw_input()
 	plt.imshow(image)
-	plt.imshow(p_map, cmap='seismic',interpolation='nearest', alpha=0.6)
-	plt.show();
+	plt.imshow(p_map, cmap='jet',interpolation='nearest', alpha=0.5)
+	plt.show()
+	
+
+def plot_components():
+	global components
+
+	for c in components:
+		plt.imshow(c, cmap='gray')
+		plt.show()
 
 def generate_pixel_map():
 	"""
 	generates a map of probabilities that can be overlayed on the image
 	"""
 	global p_map
+	global zones
 	p_map = np.zeros((img_height, img_width))
-	for i in range(img_height):
-		for j in range(img_width):
-			p_map[i,j] = get_pixel_zone(j,i)
+	tmp1 = img_width/np.sqrt(num_windows)
+	tmp2 = img_height/np.sqrt(num_windows)
+	for i in range(zones.shape[0]):
+		for j in range(zones.shape[1]):
+			p_map[i*tmp2:(i+1)*tmp2,j*tmp1:(j+1)*tmp1] = zones[i,j]
 
 def get_pixel_zone(x, y):
 	global num_windows
@@ -178,6 +219,7 @@ def get_pixel_zone(x, y):
 def normalize_map():
 	global zones
 	zones = zones/zones.sum()
+	print zones.sum()
 
 
 def set_random_p():
